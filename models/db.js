c826f3dcb3f0c60f5f29c41ac9b039890caf2ca6
ZQ-MBP <850307035@qq.com>
2018-03-18 21:18:23 +0800

    使用本地数据库

--- models/db.js
+++ models/db.js
@@ -1,23 +1,11 @@
 // 连接数据库
 const mongoose = require('mongoose');
 
-// Aliyun
-// const dbAddress1 = 'dds-bp1e4e5b9d0fb5241578-pub.mongodb.rds.aliyuncs.com:3717';
-// const dbAddress2 = 'dds-bp1e4e5b9d0fb5242256-pub.mongodb.rds.aliyuncs.com:3717';
-// const dbName = 'mydata';
-// const dbUser = 'mgdb';
-// const dbPass = 'db123123';
-// const dbCopy = 'mgset-5292433';
+// local
 
-// mlab
-const dbAddress1 = 'ds237748.mlab.com:37748';
-// const dbAddress2 = 'dds-bp1e4e5b9d0fb5242256-pub.mongodb.rds.aliyuncs.com:3717';
-const dbName = 'mdb';
-const dbUser = 'fst';
-const dbPass = 'testsdo0921';
-// const dbCopy = 'mgset-5292433';
+const dbConnectString = 'mongodb://ZSQ:mongoZSQ@localhost/fullStackDemo';
 
-const dbConnectString = `mongodb://${dbUser}:${dbPass}@${dbAddress1}/${dbName}`;
+// const dbConnectString = `mongodb://${dbUser}:${dbPass}@${dbAddress1}/${dbName}`;
 
 const db = mongoose.connect(dbConnectString)
   .then(dt => console.log('database connect success~'))
